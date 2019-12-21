a2c91e69e71c4b0bd1b801f725ef073c07ba65a7
Veronika Dodda <v.shengeliya@gmail.com>
2019-12-21 03:36:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,7 +21,7 @@ end
   
 
   def list(songs)
-      songs.each_with_index {|song , index| puts "#{index +1}. #{song}"}
+      songs.each_with_index {|song, index| puts "#{index +1}. #{song}"}
   end 
   
   
